ea439bb8bf006e329130625bc401c9737e5da000
SangYoungBaek <bsy2274@naver.com>
2023-08-31 11:27:01 +0900

    로그인 구현

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,5 +35,18 @@ public class UserController {
         userService.signup(requestDto);
 
         return "redirect:/api/user/login-page";
+    }
+
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+        //오류가 발생했다면 로그인 페이지에 에러 표시를 해줘야한다.
+        try {
+            userService.login(requestDto, res);
+        } catch (Exception e) {
+            // 이렇게 보내달라고 클라이언트에서 요청이 왔다. error를 보내준다.
+            return "redirect:/api/user/login-page?error";
+        }
+
+        return "redirect:/";
     }
 }
